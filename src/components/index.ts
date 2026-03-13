5dffe92eabbb87279757e0f9e42404d665756342
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-12 23:54:07 -0300

    feat: agrega archivo barril para components

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+
+export * from './products/ProductGrid';
+export * from './products/SizeSelector';
+export * from './products/QuantitySelector';
+export * from './products/ProductSlide';
+export * from './products/ExpandableText';
+
+export * from './ui/TopMenu';
+export * from './ui/PageNotFound';
+export * from './ui/Title';
+export * from './ui/sidebar/Sidebar';
