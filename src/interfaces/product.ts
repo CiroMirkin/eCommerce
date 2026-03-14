67ff18b2a29fd1f37c8b7012581443ac739aef0a
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-14 16:44:50 -0300

    refactor: agregar mapeos de categoria en interfaz de producto

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -9,9 +9,24 @@ export interface Product {
     slug: string;
     tags: string[];
     title: string;
-    type?: ProductTypes;
+    type: ProductTypes;
 }
 
 export type Size = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
 
 export type ProductTypes = "women" | "men" | "shirts" | "tank-tops" | "hoodies" | "jackets" | "polo-shirts" | "hats";
+
+export const productTypeToCategory: Record<ProductTypes, string> = {
+    'women': 'Women',
+    'men': 'Men',
+    'shirts': 'Shirts',
+    'tank-tops': 'Tank Tops',
+    'hoodies': 'Hoodies',
+    'jackets': 'Jackets',
+    'polo-shirts': 'Polo Shirts',
+    'hats': 'Hats'
+}
+
+export const categoryToProductType = Object.fromEntries(
+    Object.entries(productTypeToCategory).map(([key, value]) => [value, key])
+) as Record<string, ProductTypes>
