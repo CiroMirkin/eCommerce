3f88dcf96d9ec6ec1a960568c28947c1609cb3c5
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-14 08:16:00 -0300

    feat: implementar paginacion en pagina de productos

--- src/app/(shop)/products/page.tsx
+++ src/app/(shop)/products/page.tsx
@@ -1,13 +1,26 @@
-import { ProductGrid } from "@/components";
-import { initialData } from "@/seed/seed";
+import { Pagination, ProductGrid } from "@/components";
+import { getPaginatedProductsWithImages } from "@/actions";
+import { redirect } from "next/navigation";
 
-const products =initialData.products
+interface Props {
+    searchParams: Promise<{
+        page?: string
+    }>
+}
 
+export default async function ProductsPage({ searchParams }: Props){
+    const pageParam = (await searchParams).page
+    const page = pageParam ? parseInt(pageParam) : 1
+    const { products, totalPages } = await getPaginatedProductsWithImages({ page })
+    
+    if(products.length === 0) {
+        redirect('/products')
+    }
 
-export default function ProductsPage(){
     return (
         <>
             <ProductGrid products={products} />
+            <Pagination totalPages={totalPages} />
         </>
     )
 }
