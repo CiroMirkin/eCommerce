f8cc5ae8c8024c49917c9f1ccb67ae342f8933f6
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-14 21:28:11 -0300

    feat: integrar categorias dinamicas en ShopLayout

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,16 +1,18 @@
+import { getCategories } from "@/actions/product/category";
 import { Footer, Sidebar, TopMenu } from "@/components";
 
-export default function ShopLayout({
+export default async function ShopLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    return (
-        <main className="w-full md:max-w-5xl 2xl:max-w-7xl mx-auto">
-          <TopMenu />
-          <Sidebar />
-          { children }
-          <Footer className="mt-10" />
-        </main>
-    )
+  const { categories } = await getCategories()
+  return (
+    <main className="w-full md:max-w-5xl 2xl:max-w-7xl mx-auto">
+      <TopMenu />
+      <Sidebar categories={categories} />
+      { children }
+      <Footer className="mt-10" />
+    </main>
+  )
 }
