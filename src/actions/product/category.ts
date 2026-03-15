6e492da9aa96882aa74c25485cd90677e81df257
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-14 21:27:59 -0300

    feat: agregar funcion getCategories para obtener categorias desde BD

---
+++ src/actions/product/category.ts
@@ -0,0 +1,24 @@
+import { categoryToProductType, ProductTypes } from "@/interfaces"
+import { prisma } from "@/lib/prisma"
+
+interface Response {
+    categories: { 
+        type: ProductTypes
+        category: string
+     }[]
+}
+
+export const getCategories = async (): Promise<Response> => {
+    try {
+        const categories = await prisma.category.findMany({})
+        return {
+            categories: categories.map(category => ({
+              type: categoryToProductType[category.name],
+              category: category.name,
+            })),
+        }
+    } catch (error) {
+        console.error("getCategories() ", error)
+        throw new Error('No fue posible cargar las categorías disponibles.')
+    }
+}
