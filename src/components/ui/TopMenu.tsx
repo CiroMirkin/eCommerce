995ddd06185b9a5658092ca9f950d73b62621061
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-15 11:57:07 -0300

    feat: agregar badge con cantidad de productos en TopMenu

--- src/components/ui/TopMenu.tsx
+++ src/components/ui/TopMenu.tsx
@@ -5,10 +5,20 @@ import Link from "next/link";
 import { useSideMenuStore } from "./sidebar/stores";
 import { titleFont } from "@/config/fonts";
 import { cn } from "@/lib/cn";
+import { useCartStore } from "@/store";
+import { useSyncExternalStore } from "react";
 
 export function TopMenu(){
     const openSideMenu = useSideMenuStore(state =>  state.openSideMenu)
+    const { getTotalProductsInCart } = useCartStore()
 
+     const total = useSyncExternalStore(
+        useCartStore.subscribe,
+        () => getTotalProductsInCart(),
+        () => 0
+    )
+    const showBadge = total > 0
+    
     return (
         <nav className="w-full py-10 flex items-center justify-between">
             <ul className="flex gap-4 sm:gap-6">
@@ -33,12 +43,25 @@ export function TopMenu(){
                     </Link>
                 </li>
                 <li>
-                    <Link href='/cart' className="font-semibold hover:text-primary hover:underline transition-colors duration-100 ease-in">
+                    <Link href='/cart' className="group font-semibold hover:text-primary hover:underline transition-colors duration-100 ease-in">
                         <div className="relative block sm:hidden">
-                            <span className="absolute px-1 text-xs rounded-full font-bold -top-2 -right-2 bg-black text-white"></span>
-                            <ShoppingCart className=""/>
+                            { showBadge && (
+                                <span className={cn(
+                                    "absolute py-px px-[2.5px] text-xs rounded-full font-bold -top-1 -right-2.5 bg-black text-white group-hover:bg-primary group-hover:text-black transition-colors",
+                                    total < 10 ? "px-1.5" : "px-[2.5px]",
+                                )}>{ total }</span>
+                            )}
+                            <ShoppingCart />
                         </div>
-                        <span className="hidden sm:block">Cart</span>
+                        <div className="relative hidden sm:block">
+                            { showBadge && (
+                                <span className={cn(
+                                    "absolute py-px px-[2.5px] text-xs rounded-full font-bold -top-2 -right-3.5 bg-white group-hover:bg-primary text-black transition-colors",
+                                    total < 10 ? "px-1.5" : "px-[2.5px]",
+                                )}>{ total }</span>
+                            )}
+                            <span>Cart</span>
+                        </div>
                     </Link>
                 </li>
                 <li>
