0ce6d97e93a6f36c26ba167aa0052ffc161c40da
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-15 17:02:12 -0300

    feat: actualizar componentes del carrito

--- src/app/(shop)/cart/ProductsInCart.tsx
+++ src/app/(shop)/cart/ProductsInCart.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 import { useSyncExternalStore } from "react"
 
 export default function ProductsInCart() {
-    const { updateProductQuantity } = useCartStore()
+    const { updateProductQuantity, removeProductFromCart } = useCartStore()
     const productsInCart = useSyncExternalStore(
         useCartStore.subscribe,
         () => useCartStore.getState().cart,
@@ -25,21 +25,29 @@ export default function ProductsInCart() {
                     className="mr-4 rounded"
                 />
                 <div className="w-full flex justify-between gap-2">
-                    <div>
-                        <Link 
-                            href={`/product/${ product.slug }`}
-                            className="text-base font-bold mb-1 hover:underline transition-all duration-300"
-                        >
-                            { product.title }
-                        </Link>
-                        <p className="text-sm font-semibold">${ product.price }</p>
+                    <div className="flex flex-col justify-between">
+                        <div>
+                            <Link 
+                                href={`/product/${ product.slug }`}
+                                className="text-base font-bold mb-1 hover:underline transition-all duration-300"
+                            >
+                                { product.title }
+                            </Link>
+                            <p className="text-sm font-semibold">${ product.price }</p>
+                        </div>
+                        <p className="text-xs font-semibold">Size: { product.size }</p>
                     </div>
                     <div className="flex flex-col justify-between">
                         <QuantitySelector
                             quantity={product.quantity}
                             handleQuantityChange={(quantity: number) => updateProductQuantity(product, quantity)}
                         />
-                        <button className="text-sm py-1 px-2 border border-black rounded hover:border-mist-600 hover:bg-dark transition-colors duration-500">Remove</button>
+                        <button
+                            onClick={() => removeProductFromCart(product)}
+                            className="text-sm py-1 px-2 border border-black rounded hover:border-mist-600 hover:bg-dark transition-colors duration-500"
+                        >
+                                Remove
+                        </button>
                     </div>
                 </div>
             </div>
