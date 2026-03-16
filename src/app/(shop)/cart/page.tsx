adf70cb3d3ac23bc40c452e738cf3d1d20f9e3ee
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-16 06:45:17 -0300

    feat: agregar resumen de orden a la pagina de carrito

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,9 +1,7 @@
 import { Title } from "@/components";
-import { titleFont } from "@/config/fonts";
-import { cn } from "@/lib/cn";
-import Link from "next/link";
 import ProductsInCart from "./ProductsInCart";
 import ProductsQuantity from "./ProductsQuantity";
+import OrderSummary from "./OrderSummary";
 
 export default function CartPage() {
     return (
@@ -14,15 +12,7 @@ export default function CartPage() {
                     <ProductsInCart />
                 </header>
                 <footer className="w-full lg:flex-1 lg:max-w-sm min-h-35 p-2 px-10 grid place-items-center bg-white text-black rounded shadow-xl lg:sticky lg:top-8">
-                    <div className="w-full">
-                        <h2 className={cn("text-xl flex justify-between mb-6", titleFont.className)}>
-                            <span>Total</span> <span className="font-bold">${ 405 }</span>
-                        </h2>
-                        <Link
-                            href='/checkout/address'
-                            className="btn-primary w-70"
-                        >Proceed to checkout</Link>
-                    </div>
+                    <OrderSummary />
                 </footer>
             </div>
         </div>
