c98d1d327e6d8ce530f55c8db61ca43577a97a17
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-16 18:21:29 -0300

    feat: crear accion de autenticacion

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,8 @@
 
+// Product Actions
 export * from '@/actions/product/product-pagination'
 export * from '@/actions/product/product-by-slug'
 export * from '@/actions/product/stock-by-slug'
 
+// Auth Actions
+export * from '@/actions/auth/login'
