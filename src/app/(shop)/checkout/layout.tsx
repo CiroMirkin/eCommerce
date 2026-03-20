2654100501ed9e08624428413c18c09ff6a4a645
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-19 22:42:20 -0300

    fix: corregir logica de proteccion de rutas en checkout

--- src/app/(shop)/checkout/layout.tsx
+++ src/app/(shop)/checkout/layout.tsx
@@ -5,8 +5,8 @@ import { redirect } from "next/navigation";
 
 export default async function CheckoutLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const session =  await auth()
-
-  if(session?.user){
+  
+  if(!session?.user){
     redirect('/auth/login?redirectTo=/checkout/address')
   }
 
