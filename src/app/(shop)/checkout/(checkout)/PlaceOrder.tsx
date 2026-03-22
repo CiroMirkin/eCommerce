3433beb0824966f52cd12773232cbb3e0433b269
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-22 11:19:45 -0300

    refactor: integrar placeOrder y clearCart en componente PlaceOrder

--- src/app/(shop)/checkout/(checkout)/PlaceOrder.tsx
+++ src/app/(shop)/checkout/(checkout)/PlaceOrder.tsx
@@ -1,34 +1,61 @@
 "use client"
 
+import { placeOrder } from "@/actions"
 import { Spinner } from "@/components"
 import { titleFont } from "@/config/fonts"
 import { cn } from "@/lib/cn"
 import { useAddressStore, useCartStore } from "@/store"
 import { currencyFormat } from "@/utils"
+import { gooeyToast, GooeyToaster } from "goey-toast"
+import { notFound, useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function PlaceOrder() {
+    const router = useRouter()
+    
     const [ isPlacingOrder, setIsPlacingOrder ] = useState(false)
+    
     const { address } = useAddressStore()
-    const { getTotalProductsInCart, getSummaryInformation, cart } = useCartStore()
+    const { getSummaryInformation, cart, clearCart } = useCartStore()
     const productsInTheOrder = getSummaryInformation().productsInCart
-    const total = getTotalProductsInCart()
+    const total =  getSummaryInformation().subTotal
 
-    /*  href='/orders/123' */
+    if(!productsInTheOrder && !isPlacingOrder) notFound()
+
     const handlePlaceOrder = async () => {
         setIsPlacingOrder(true)
-        
+    
         const productsToOrder = cart.map(product => ({
             productId: product.id,
             quantity: product.quantity,
             size: product.size,
         }))
 
-        console.log(productsToOrder)
+        const response = await placeOrder(productsToOrder, {
+            firstName: address.firstName,
+            lastName: address.lastName,
+            address: address.address,
+            city: address.city,
+            zipCode: address.zipCode,
+            telephone: address.telephone,
+            secondAddress: address.secondAddress,
+            country: address.country,
+        })
+
+        if(!response.ok) {
+            setIsPlacingOrder(false)
+            gooeyToast.error(response.message)
+            return
+        }
+        gooeyToast.success(response.message)
+        
+        clearCart()
+        router.replace('/orders/'+response.order!.id)
     }
 
     return (
         <>
+        <GooeyToaster position="top-right" />
             <div className="w-full">
                 <h2 className={cn("text-xl mb-2", titleFont.className)}>Address</h2>
                 <ul className="list-disc pl-4">
