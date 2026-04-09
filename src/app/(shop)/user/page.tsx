75d9372884c0099362fa2aab062fdc714195300c
Ciro Mirkin <ciromirkin@gmail.com>
2026-04-09 19:58:06 -0300

    feat: agregar enlaces a paneles admin en pagina de usuario

--- src/app/(shop)/user/page.tsx
+++ src/app/(shop)/user/page.tsx
@@ -14,9 +14,16 @@ export default async function UserPage() {
 
     return (
         <>
-            <Title>Profile</Title>
+            <Title>Hi, { session.user.name }</Title>
             <div className="my-3">
-                <Link href="/orders" className="hover:text-primary hover:underline">My orders</Link>
+                {
+                    session.user.role === 'admin' 
+                    ? <div className="flex gap-4 flex-wrap">
+                        <Link href="/admin/orders" className="hover:text-primary hover:underline">See orders</Link>
+                        <Link href="/admin/users" className="hover:text-primary hover:underline">See users</Link>
+                    </div>
+                    : <Link href="/orders" className="hover:text-primary hover:underline">My orders</Link>
+                }
             </div>
             <LogoutButton />
         </>
