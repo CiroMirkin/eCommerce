6388d4fba7b831e4dcbd8bc8c53596d05237aa4f
Ciro Mirkin <ciromirkin@gmail.com>
2026-04-11 18:41:07 -0300

    feat: proteccion de rutas de administrador

---
+++ src/app/(shop)/admin/layout.tsx
@@ -0,0 +1,14 @@
+"use server"
+
+import { auth } from "@auth";
+import { redirect } from "next/navigation";
+
+export default async function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    const session =  await auth()
+  
+    if(session?.user.role !== 'admin') {
+        redirect('/login')
+    }
+
+    return ( <> { children } </> )
+}
